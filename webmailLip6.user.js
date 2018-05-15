b38335829ce770e873c3fd07468345a9f0a0f9e5
Thibault Suzanne <thi.suzanne@gmail.com>
2018-05-15 12:51:43 +0200

    Wip

--- webmailLip6.user.js
+++ webmailLip6.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name        Liste Noire
-// @namespace   https://github.com/thizanne/userscripts/webmail_lip6
+// @name        Webmail LIP6
+// @namespace   https://github.com/thizanne/webmail_lip6
 // @description Choisit automatiquement l'équipe APR dans le webmail LIP6
 // @author      Thibault Suzanne
 // @include     webmail.lip6.fr/horde/login.php
