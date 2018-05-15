37bd2aae2ab97a8adbe599f4acfe4a000285e6ef
Thibault Suzanne <thi.suzanne@gmail.com>
2018-05-15 12:49:21 +0200

    Initial commit

---
+++ webmailLip6.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        Liste Noire
+// @namespace   https://github.com/thizanne/userscripts/webmail_lip6
+// @description Choisit automatiquement l'équipe APR dans le webmail LIP6
+// @author      Thibault Suzanne
+// @include     webmail.lip6.fr/horde/login.php
+// @version     0.1
+// ==/UserScript==
+
+alert("lol");
